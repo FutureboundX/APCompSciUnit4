0710f03692660db2086e3817f6818f22487747d3
FutureboundX <48761556+FutureboundX@users.noreply.github.com>
2019-12-20 07:58:17 -0500

    Create arrayNotes.java

---
+++ arrayNotes.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+public class arrayPractice {
+	
+	public static void main(String[] args) {
+		//Do now: Declare an array called array1 with 10 integer elements
+		//Initialize the array 
+//		int[] array1 = new int[10];
+//		array1[0] = 1;
+//		array1[1] = 2;
+//		array1[2] = 3;
+//		array1[3] = 4;
+//		array1[4] = 5;
+//		array1[5] = 6;
+//		array1[6] = 7;
+//		array1[7] = 8;
+//		array1[8] = 9;
+//		array1[9] = 10;
+		//Print integer 5
+		//System.out.println(array1[4]);
+		
+		
+		//Another way to declare and initialize an array
+		//int[] studentGrades = {95, 52, 87};
+		//Print the first element
+		//System.out.println(studentGrades[0]);
+		//System.out.println("Student #1 got a grade of " + studentGrades[0] + " on his test.");
+		
+		
+		//Can't re-size arrays
+//		int[] studentAges = new int [3];
+//		studentAges[0] = 15;
+//		studentAges[1] = 16;
+//		studentAges[2] = 17;
+//		
+//		studentAges = new int[5];
+//		studentAges[3] = 19;
+//		studentAges[4] = 21;
+//		
+//		System.out.println(studentAges[0]);
+		
+		
+		//Declare and initialize a string array
+		//String[] guests = {"Anthony", "Kyle", "Fabian", "Dylan"};
+		//Changing elements is allowed
+		//guests[1] = "Kevin";
+		//System.out.println("Kyle got eliminated out for " + guests[1]);
+		
+		
+		
+//		int[] num = {1, 2, 3, 4, 5};
+//		String[] iceCream = {"Chocolate", "Vanilla", "Strawberry"};
+//		System.out.println("We bought " + num[0] + " " + iceCream[1] + " ice creams.");
+		
+		
+		//Declare and initalize a 2 by 3 array
+		//int [][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
+		
+//		//Loop through all elements and print them out
+//		for (int r = 0; r < matrix.length; r++){
+//			for (int c = 0; c < matrix[r].length; c++){
+//				System.out.print(matrix[r][c] + " ");
+//			}
+//			System.out.println();
+//		}
+		//print2D(matrix);
+		//System.out.println(matrix[1][1]);
+	//}
+	//Method for printing 2D array
+	//public static void print2D(int[][] array){
+		//for (int r = 0; r < array.length; r++){
+			//for (int c = 0; c < array[r].length; c++){
+				//System.out.print(array[r][c] + " ");
+			//}
+			//System.out.println();
+		//}
+		
+		//Sums all rows and columns in a 2D array
+//		int [][] intArray = {{5,6,2,4}, {10,9,3,1}};
+//		int sum = 0;
+//		for (int r = 0; r < intArray.length; r++){
+//			for (int c = 0; c < intArray[r].length; c++){
+//				sum += intArray[r][c];
+//			}
+//		}
+//		System.out.println(sum);
+		
+		
+	}
+}
